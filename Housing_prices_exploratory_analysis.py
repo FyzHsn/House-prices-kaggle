0556ac55adde6731de50b91e8702998783b07b5c
Faiyaz Hasan <faiyaz.hasan1@gmail.com>
2016-10-25 13:31:28 -0400

    Updated script with correlation matrix
    Figured out which numerical features have the highest correlation score with the sale price.

--- Housing_prices_exploratory_analysis.py
+++ Housing_prices_exploratory_analysis.py
@@ -31,9 +31,11 @@ df.columns # Column names - SalePrice is the target variable
 
 df.columns = [colnames.lower() for colnames in df.columns] # lowercase
 
-#########################
-# 2. DATA PREPROCESSING #
-#########################
+#####################################################
+# 2. EXPLORATORY DATA ANALYSIS + DATA PREPROCESSING #
+#####################################################
+y = df['saleprice'].values
+
 plt.figure()
 plt.subplot(211)
 plt.hist(y, bins=30, facecolor='green', alpha=0.75)
@@ -50,7 +52,7 @@ plt.tight_layout()
 #plt.clf()
 plt.show()
 
-df_new = df # Create new set of log transformed data
+df_new = df.copy() # Create new set of log transformed data
 df_new['saleprice'] = np.log1p(df['saleprice'])
 
 
@@ -62,10 +64,55 @@ skewed_feats = skewness[skewness > 0.75].index
 # log transform skewed feature vectors
 df_new[skewed_feats] = np.log1p(df[skewed_feats])
 
+# break down numeric features into 4 batches for visibility - 32 cols.
+numeric_feats = list(numeric_feats.difference(['id', 'saleprice']))
+batch1 = numeric_feats[0:9]
+batch2 = numeric_feats[9:18]
+batch3 = numeric_feats[18:27]
+batch4 = numeric_feats[27:36]
+batch1.append('saleprice')
+batch2.append('saleprice')
+batch3.append('saleprice')
+batch4.append('saleprice')
+
+# correlation matrix of numeric features
+cor_mat = np.corrcoef(df_new[batch4].values.T)
+sns.set(font_scale=1.5)
+heat_map = sns.heatmap(cor_mat,
+                       cbar=True,
+                       annot=True,
+                       fmt='.2f',
+                       annot_kws={'size': 12},
+                       yticklabels=batch4,
+                       xticklabels=batch4)
+plt.title('Correlation Matrix - Batch 4 columns')
+plt.show()
+
+"""
+Variables of interest in prediction model based on correlation coeffs.
+1. overallqual - 0.82
+2. grlivarea - 0.73
+3. garagecars - 0.68
+4. garagearea - 0.65
+5. 1stflrsf - 0.61
+6. fullbath - 0.59
+7. yearbuilt - 0.59
+8. yearremodadd - 0.57
+9. totrmsabvgrd - 0.53
+10. fireplaces - 0.49
+11. openporchsf - 0.46
+12. lotarea - 0.40
+13. totalbsmtsf - 0.37
+14. wooddecksf - 0.34
+15. halfbath - 0.31
+
+"""
+
 # get dummy variables for nominal variables
 df_new = pd.get_dummies(df_new)
 
-X = df_new.iloc[:, !37].values # Matrix containing log transformed skewed features
+X = \
+df_new.loc[:, [i for i in skewed_feats.difference(['saleprice'])]].values # Matrix containing log transformed skewed features
 y = df_new['saleprice'].values # Target variable - log of sale price of house
 
 # missing values - Impute via mean
@@ -73,24 +120,9 @@ df_new[numeric_feats].isnull().sum()
 imr = Imputer(missing_values='NaN', strategy='mean', axis=0)
 df_new = imr.fit_transform(df_new[numeric_feats])
 
-################################
-# 3. EXPLORATORY DATA ANALYSIS #
-################################
-numeric_feats = numeric_feats[1:]
 
-# correlation matrix of numeric features
-cor_mat = np.corrcoef(df_new[numeric_feats].values.T)
-sns.set(font_scale=1.5)
-heat_map = sns.heatmap(cor_mat,
-                       cbar=True,
-                       annot=True,
-                       square=True,
-                       fmt='.2f',
-                       annot_kws={'size': 12},
-                       yticklabels=numeric_feats,
-                       xticklabels=numeric_feats)
-plt.title('Correlation Matrix - Heat Map')
-plt.show()
+
+
 
 
 
